a4a3e0cb06be81913cb0f7c2d4f37d6796ccd965
Flora <flora.official.app@gmail.com>
2026-02-11 18:08:42 +0900

    Fix astro config for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'server',
-
   vite: {
     plugins: [tailwindcss()]
   },
-
   adapter: vercel()
 });
