fe5d3403300298b2933509008742abf1252e6225
Flora <141000543+zv4s8ld3@users.noreply.github.com>
2026-03-12 11:05:31 +0900

    Update astro.config.mjs
    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'server',
-  vite: {
-    plugins: [tailwindcss()]
-  },
-  adapter: vercel(),
-  image: {
-    service: { entrypoint: 'astro/assets/services/noop' },
-  },
+output: 'server',
+adapter: vercel(),
+
+vite: {
+plugins: [tailwindcss()]
+}
 });
